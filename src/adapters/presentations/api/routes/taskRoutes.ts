48978b5dddc20b890237083055b42bcbac433a8e
EmersonLaranja <emersonlaranja@gmail.com>
2024-03-06 09:00:11 -0300

    refatoração da aula 2

--- src/adapters/presentations/api/routes/taskRoutes.ts
+++ src/adapters/presentations/api/routes/taskRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { expressRouteAdapter } from "../../../expressRouteAdapter";
-import { taskControllerFactory } from "../../factories/taskControllerFactory";
+import { taskControllerFactory } from "../../../factories/taskControllerFactory";
 
 export default (router: Router): void => {
   router.post("/tasks", expressRouteAdapter(taskControllerFactory()));
