4e6df940d02986fcf6a2179eb2ba16aa385433e7
EmersonLaranja <emersonlaranja@gmail.com>
2024-03-07 16:43:54 -0300

    nome errado

--- src/dataSources/db/repository/taskMongoRepository.ts
+++ src/dataSources/db/repository/taskMongoRepository.ts
@@ -1,10 +1,10 @@
 import { Task } from "../../../entities/task/task";
-import { AddATaskModel } from "../../../usecases/addTask";
+import { AddTaskModel } from "../../../usecases/addTask";
 import { AddTaskRepository } from "../../../usecases/repository/addTaskRepository";
 import { MongoDbManager } from "../config/mongoDbManager";
 
 export default class TaskMongoRepository implements AddTaskRepository {
-  async add(taskData: AddATaskModel): Promise<Task> {
+  async add(taskData: AddTaskModel): Promise<Task> {
     const taskCollection = MongoDbManager.getInstance().getCollection("tasks");
 
     const { insertedId } = await taskCollection.insertOne(taskData);
